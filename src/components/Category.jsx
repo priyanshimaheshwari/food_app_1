63ffbdc02579394c691f2c1434fe818a2f17022e
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2022-12-24 18:29:12 +0530

    new

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { categories } from '../data/data'
 
 const Category = () => {
+    console.log(categories)
   return (
-    <div>Category</div>
+    <div>
+        <h1>Top Rated Menu Items</h1>
+        {/* Categories */}
+        <div>
+            {categories.map((item, index) =>(
+                <div key={index}>
+                    <h2></h2>
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
 
