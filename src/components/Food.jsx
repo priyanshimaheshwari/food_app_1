931dc59f3b98cf728f35672e404d07662345153e
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2022-12-24 18:08:13 +0530

    new

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -41,7 +41,7 @@ const Food = () => {
                    <img src={item.image} alt={item.name} className='w-full h-[200px] object-cover rounded-t-lg'/>
                    <div className='flex justify-between px-2 py-4 font-bold text-gray-500'>
                        <p>{item.name}</p>
-                       <p><span>{item.price}</span></p>
+                       <p><span className='bg-orange-500 text-white p-1 rounded-full'>{item.price}</span ></p>
                    </div>
                </div>
             ))}
