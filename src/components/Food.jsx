53203fa02cbf5a8be383392ddbb93a0abb906002
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2022-12-24 17:58:01 +0530

    new

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,36 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {data} from '../data/data.jsx'
 
 const Food = () => {
-    console.log(data);
+    //console.log(data);
+    const [foods,setFoods]=useState(data)
   return (
     <div className='max-w-[1640px] m-auto px-4 py-12'>
-        <h1 className='text-orange-600'>Top Rated Menu Items</h1>
+        <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
 
         {/*Filter Row*/}
         <div>
             {/*Filter Type*/}
             <div>
-                 <p>Filter Type</p>
+                 <p className='font-bold text-gray-700'>Filter Type</p>
                  <div>
-                     <button>All</button>
-                     <button>Burgers</button>
-                     <button>Pizzas</button>
-                     <button>Salads</button>
-                     <button>Pastas</button>
+                     <button className='m-1 border-orange-900 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
+                     <button className='m-1 border-orange-900 text-orange-600 hover:bg-orange-600 hover:text-white'>Burgers</button>
+                     <button className='m-1 border-orange-900 text-orange-600 hover:bg-orange-600 hover:text-white'>Pizzas</button>
+                     <button className='m-1 border-orange-900 text-orange-600 hover:bg-orange-600 hover:text-white'>Salads</button>
+                     <button className='m-1 border-orange-900 text-orange-600 hover:bg-orange-600 hover:text-white'>Pastas</button>
                  </div>
             </div>
 
             {/*Filter Price*/}
             <div>
-                 <p>Filter Price</p>
-                 <div>
-                     <buttons>$</buttons>
-                     <buttons>$$</buttons>
-                     <buttons>$$$</buttons>
-                     <buttons>$$$$</buttons>
+                 <p className='font-bold text-gray-700'>Filter Price</p>
+                 <div className='flex justify-between max-w-[390px] w-full'>
+                 <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$</button>
+                     <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$</button>
+                     <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$</button>
+                     <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$$</button>
                  </div>
             </div>
+        </div>
+
+        {/*display foods */}
+        <div className=''>
+            {foods.map((item, index)=>(
+               <div key={index}>
+                   <img src={item.image} alt={item.name} />
+                   <div>
+                       <p>{item.name}</p>
+                       <p><span>{item.price}</span></p>
+                   </div>
+               </div>
+            ))}
+
         </div>
     </div>
   )
