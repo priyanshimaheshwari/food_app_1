be8077e4aee86711f201d19b81fb4c84ac73f1aa
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2022-12-25 22:50:48 +0530

    new

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -13,7 +13,8 @@ const Category = () => {
         {categories.map((item, index) => (
           <div
             key={index}
-            className='bg-gray-100 rounded-lg p-4 flex justify-between items-center hover:scale-205 hover:bg-slate-500'
+            className='bg-gray-100 rounded-lg p-4 flex justify-between items-center hover:scale-105
+            duration-300 hover:bg-slate-500'
           >
             <h2 className='font-bold sm:text-xl '>{item.name}</h2>
             <img src={item.image} alt={item.name} className='w-20' />
